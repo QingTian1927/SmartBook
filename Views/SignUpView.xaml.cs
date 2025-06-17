418a97d51ebc82360d41e0b84f09f19882e23463
QingTian <qingtian63910@proton.me>
2025-06-17 16:16:06 +0700

    feat: created the Sign Up page.

---
+++ Views/SignUpView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SmartBook.Views;
+
+public partial class SignUpView : Page
+{
+    public SignUpView()
+    {
+        InitializeComponent();
+    }
+
+    private void BtnCreateAccount_Click(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void BtnBackToLogin_Click(object sender, RoutedEventArgs e)
+    {
+        MainWindow.Instance.Title = "SmartBook - Sign Up";
+        MainWindow.Instance.Navigate(new LoginView());
+    }
+}
