4916446c117d322406babbf15c048e462d52ce4b
QingTian <qingtian63910@proton.me>
2025-06-17 16:16:30 +0700

    refactor: updated login view.

--- Views/LoginView.xaml.cs
+++ Views/LoginView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using SmartBook.Utils;
 
 namespace SmartBook.Views;
 
@@ -9,9 +10,40 @@ public partial class LoginView : Page
     {
         InitializeComponent();
     }
-    
-    private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+
+    private void BtnSignIn_Click(object sender, RoutedEventArgs e)
     {
-        throw new NotImplementedException();
+        string email = txtEmail.Text;
+        string password = txtPassword.Password;
+
+        if (!Validator.IsValidEmail(email))
+        {
+            MessageBox.Show(
+                "Please enter a valid email address",
+                "Login Error",
+                MessageBoxButton.OK,
+                MessageBoxImage.Warning
+            );
+            return;
+        }
+
+        if (!Validator.IsValidPassword(password))
+        {
+            MessageBox.Show(
+                "Invalid password.\nPassword must be 3 to 32 characters long and can include letters, numbers, and special characters (!@#$%^&*()-_=+[]{}|:;'\",.<>?/).",
+                "Login Error",
+                MessageBoxButton.OK,
+                MessageBoxImage.Warning
+            );
+            return;
+        }
+        
+        
+    }
+
+    private void BtnRegister_Click(object sender, RoutedEventArgs e)
+    {   
+        MainWindow.Instance.Title = "SmartBook - Login";
+        MainWindow.Instance.Navigate(new SignUpView());
     }
 }
