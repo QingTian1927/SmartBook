5256f7f7709e954610b749027b7b6da8d7b97c7e
QingTian <qingtian63910@proton.me>
2025-06-19 01:27:51 +0700

    feat: added logout.

--- Views/Components/SidebarView.xaml.cs
+++ Views/Components/SidebarView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using SmartBook.Core.Data;
 
 namespace SmartBook.Views.Components;
 
@@ -12,15 +13,26 @@ public partial class SidebarView : UserControl
 
     public string CurrentUsername
     {
-        get { return (string)GetValue(CurrentUsernameProperty); }
-        set { SetValue(CurrentUsernameProperty, value); }
+        get => (string)GetValue(CurrentUsernameProperty);
+        set => SetValue(CurrentUsernameProperty, value);
     }
-    
-    public static readonly DependencyProperty CurrentUsernameProperty = DependencyProperty.Register(nameof(CurrentUsername), typeof(string), typeof(SidebarView), new PropertyMetadata(string.Empty));
+
+    public static readonly DependencyProperty CurrentUsernameProperty =
+        DependencyProperty.Register(nameof(CurrentUsername), typeof(string), typeof(SidebarView),
+            new PropertyMetadata(string.Empty));
 
     private void LogoutBtn_Click(object sender, RoutedEventArgs e)
     {
-        throw new NotImplementedException();
+        ContextManager.CurrentUser = null;
+        ContextManager.IsAdmin = false;
+
+        MainWindow.Instance.Navigate(new LoginView());
+        MessageBox.Show(
+            "You are logged out!",
+            "Logout Successful",
+            MessageBoxButton.OK,
+            MessageBoxImage.Information
+        );
     }
 
     private void SettingsBtn_Click(object sender, RoutedEventArgs e)
